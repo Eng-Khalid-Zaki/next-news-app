b4e45fecbd51784629883cd5e706bbcefce4808b
Khalid Zaki <khalid@Khalid.khalid>
2025-01-22 19:39:10 +0200

    Modify: the page of filters of the news, so that that usr can pick up a specific year and month to see the news posted in this month

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,33 +1,64 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
-import { notFound } from "next/navigation";
-import React from "react";
 
 function FilteredNewsPage({ params }) {
   const filter = params.filter;
-  const newsYear = filter[0];
-  const years = getAvailableNewsYears();
-  const news = getNewsForYear(newsYear);
 
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter.");
+  }
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {years.map((year) => (
-              <li key={year}>
-                <Link href={`/archive/${year}`}>{year}</Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>{" "}
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
-      {news.length > 0 ? (
-        <NewsList news={news} />
-      ) : (
-        <p>No news for year {newsYear}</p>
-      )}
+      {newsContent}
     </>
   );
 }
